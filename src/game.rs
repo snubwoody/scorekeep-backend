2064eb60804228acd457fd170941044695eb0bc9
waku <wakunguma13@gmail.com>
2025-06-13 07:56:29 +0200

    change: move all game related code into a game service object

--- src/game.rs
+++ src/game.rs
@@ -16,68 +16,81 @@ struct Player {
     points: i32,
 }
 
-pub async fn create_game(pool: &sqlx::PgPool, name: &str) -> Game {
-    let row = sqlx::query!("INSERT INTO games(name) VALUES ($1) RETURNING *", name)
-        .fetch_one(pool)
-        .await
-        .unwrap();
-
-    let game = Game {
-        id: row.id,
-        name: row.name,
-        players: Vec::new(),
-    };
-
-    game
+#[derive(Clone)]
+pub struct GameService{
+    pool: sqlx::PgPool,
 }
 
-pub async fn get_game(pool: &sqlx::PgPool, id: Uuid) -> Option<Game> {
-    let result = sqlx::query!("SELECT * FROM games WHERE id = $1", id)
-        .fetch_one(pool)
-        .await;
-
-    match result {
-        Ok(row) => {
-            let game = Game {
-                id: row.id,
-                name: row.name,
-                players: Vec::new(),
-            };
-
-            Some(game)
-        }
-        Err(e) => None,
+impl GameService{
+    pub fn new(pool: sqlx::PgPool) -> Self{
+        Self{pool}
     }
-}
 
-/// Add a player to a game
-pub async fn add_player(pool: &sqlx::PgPool, game_id: Uuid, user_id: Uuid) -> crate::Result<()> {
-    sqlx::query!(
-        "INSERT INTO game_participants(game,player) VALUES ($1,$2)",
-        game_id,
-        user_id
-    )
-    .execute(pool)
-    .await?;
+    pub async fn create_game(&self,name: &str) -> Game {
+        let row = sqlx::query!("INSERT INTO games(name) VALUES ($1) RETURNING *", name)
+            .fetch_one(&self.pool)
+            .await
+            .unwrap();
 
-    Ok(())
-}
+        let game = Game {
+            id: row.id,
+            name: row.name,
+            players: Vec::new(),
+        };
 
-/// Set the game participants points
-pub async fn set_points(
-    pool: &sqlx::PgPool,
-    game_id: Uuid,
-    user_id: Uuid,
-    points: i32,
-) -> crate::Result<()> {
-    sqlx::query!(
+        game
+    }
+
+    pub async fn get_game(&self, id: Uuid) -> Option<Game> {
+        let result = sqlx::query!("SELECT * FROM games WHERE id = $1", id)
+            .fetch_one(&self.pool)
+            .await;
+
+        match result {
+            Ok(row) => {
+                let game = Game {
+                    id: row.id,
+                    name: row.name,
+                    players: Vec::new(),
+                };
+
+                Some(game)
+            }
+            Err(e) => None,
+        }
+    }
+
+    /// Add a player to a game
+    pub async fn add_player(&self,game_id: Uuid, user_id: Uuid) -> crate::Result<()> {
+        sqlx::query!(
+            "INSERT INTO game_participants(game,player) VALUES ($1,$2)",
+            game_id,
+            user_id
+        )
+            .execute(&self.pool)
+            .await?;
+
+        Ok(())
+    }
+
+    /// Set the game participants points
+    pub async fn set_points(
+        &self,
+        game_id: Uuid,
+        user_id: Uuid,
+        points: i32,
+    ) -> crate::Result<()> {
+        sqlx::query!(
         "UPDATE game_participants SET points = $1 WHERE game = $2 AND player = $3",
         points,
         game_id,
         user_id
     )
-    .execute(pool)
-    .await?;
+            .execute(&self.pool)
+            .await?;
 
-    Ok(())
+        Ok(())
+    }
 }
+
+
--- tests/games.rs
+++ tests/games.rs
@@ -1,10 +1,11 @@
 use scorekeep::create_user;
-use scorekeep::game::{self, add_player, create_game, set_points};
+use scorekeep::game::{GameService};
 use sqlx::PgPool;
 
 #[sqlx::test]
 async fn game_added_to_db(pool: PgPool) {
-    let game = create_game(&pool, "Movie Night").await;
+    let games = GameService::new(pool.clone());
+    let game = games.create_game("Movie Night").await;
     sqlx::query!("SELECT * FROM games WHERE id = $1", game.id)
         .fetch_one(&pool)
         .await
@@ -13,16 +14,18 @@ async fn game_added_to_db(pool: PgPool) {
 
 #[sqlx::test]
 async fn get_game(pool: PgPool) {
-    let game = create_game(&pool, "Movie Night").await;
-    let result = game::get_game(&pool, game.id).await;
+    let games = GameService::new(pool.clone());
+    let game = games.create_game("Movie Night").await;
+    let result = games.get_game(game.id).await;
     assert!(result.is_some());
 }
 
 #[sqlx::test]
 async fn add_player_to_game(pool: PgPool) -> scorekeep::Result<()> {
+    let games = GameService::new(pool.clone());
     let user = create_user(&pool).await;
-    let game = create_game(&pool, "My game").await;
-    add_player(&pool, game.id, user.id).await?;
+    let game = games.create_game("My game").await;
+    games.add_player(game.id, user.id).await?;
 
     let rows = sqlx::query!("SELECT * FROM game_participants WHERE game = $1", game.id)
         .fetch_all(&pool)
@@ -34,10 +37,11 @@ async fn add_player_to_game(pool: PgPool) -> scorekeep::Result<()> {
 
 #[sqlx::test]
 async fn set_players_points(pool: PgPool) -> scorekeep::Result<()> {
+    let games = GameService::new(pool.clone());
     let user = create_user(&pool).await;
-    let game = create_game(&pool, "My game").await;
-    add_player(&pool, game.id, user.id).await?;
-    set_points(&pool, game.id, user.id, 100).await?;
+    let game = games.create_game("My game").await;
+    games.add_player(game.id, user.id).await?;
+    games.set_points(game.id, user.id, 100).await?;
 
     let row = sqlx::query!("SELECT * FROM game_participants WHERE player = $1", user.id)
         .fetch_one(&pool)
